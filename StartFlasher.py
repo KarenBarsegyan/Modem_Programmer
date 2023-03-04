9c0f00b7e942ba1ff20b086e16274022f2e93178
Барсегян Карен <k.barsegyan@itelma.su>
2023-03-04 09:50:59 +0300

    Ok structure

--- StartFlasher.py
+++
@@ -1,9 +0,0 @@
-import sys
-from MainWindow import MainWindow
-from PyQt6.QtWidgets import QApplication
-
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    window = MainWindow()
-    sys.exit(app.exec())
