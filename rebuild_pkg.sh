8d7736058c2526942fbd3a86b96578a8b59f9b5f
KarenBarsegyan <karen.barsegyan-2001@bk.ru>
2023-03-30 17:17:09 +0300

    v0.0.3 + rebuild script added

--- rebuild_pkg.sh
+++ rebuild_pkg.sh
@@ -1,3 +1,24 @@
+#!/bin/sh
+
+VERSION=0.0.3
+RELEASE_NUMBER=1
+
+echo "Build pkg with version ${VERSION}"
+
+rm *.deb
+rm -r modem-programmer_*
+
+mkdir -p "modem-programmer_${VERSION}-${RELEASE_NUMBER}_all/usr/bin/"
+mkdir -p "modem-programmer_${VERSION}-${RELEASE_NUMBER}_all/DEBIAN"
+
+echo "Package: ModemProgrammer
+Version: ${VERSION}
+Maintainer: karen.barsegyan-2001@bk.ru
+Depends: libc6
+Architecture: all
+Description: SIM76 Flasher" \
+> "modem-programmer_${VERSION}-${RELEASE_NUMBER}_all/DEBIAN/control"
+
 pyinstaller --onefile -w ModemProgrammer.py
-cp dist/ModemProgrammer modem-programmer_pkg/usr/bin/ModemProgrammer 
-# dpkg --build modem-programmer_pkg
+cp "dist/ModemProgrammer" "modem-programmer_${VERSION}-${RELEASE_NUMBER}_all/usr/bin/ModemProgrammer"
+dpkg --build "modem-programmer_${VERSION}-${RELEASE_NUMBER}_all"
