52989b311c40ae2a78eb95c0c8a5f62d0abfe236
Барсегян Карен <k.barsegyan@itelma.su>
2023-05-18 16:19:46 +0300

    Startup delets files after installing FlashData

--- startup_files/startup.sh
+++ startup_files/startup.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-APT_SERVER_IP="192.168.88.10:8000"
+# APT_SERVER_IP="192.168.88.10:8000"
+APT_SERVER_IP="pi@sim7600server.local:8000"
 
 cd /home/pi/startup
 
-echo -e "\n--- Get sim7600prg ---\n"
+echo -e "\n--- Try connect to ${APT_SERVER_IP} ---"
 a=0
 while ! curl http://${APT_SERVER_IP}/apt-repo/keys/pgp-key.public > pgp-key.public; do
     echo Trying to connect $a time
@@ -17,32 +18,24 @@ while ! curl http://${APT_SERVER_IP}/apt-repo/keys/pgp-key.public > pgp-key.publ
     fi
 done
 
+echo -e "\n--- Add ${APT_SERVER_IP} to apt sources ---"
 echo "deb [arch=all signed-by=/home/pi/startup/pgp-key.public] http://${APT_SERVER_IP}/apt-repo stable main" | sudo tee /etc/apt/sources.list.d/sim7600prg.list
 
-sudo systemctl restart systemd-timesyncd.service
+echo -e "\n--- Getting Time From ${APT_SERVER_IP}---"
 
-a=0
-while [[ "$(sudo timedatectl | grep synchronized)" != \
-         "System clock synchronized: yes" ]]; do
+dateFromServer=$(curl -v --insecure --silent http://${APT_SERVER_IP} 2>&1 \
+       | grep Date | sed -e 's/< Date: //'); date +"%Y%m%d %T" -d "$dateFromServer"   
 
-    echo Trying to sync data $a time
-    a=`expr $a + 1`
-    sleep 1
-    
-    if [ $a -gt 10 ]
-    then
-    break
-    fi
+date +%Y%m%d%T -s "$dateFromServer"
 
-done
+date -u
 
-echo -e "\n--- Time Synced--- \n"
+echo -e "\n--- Get sim7600prg by apt ---"
+sudo apt-get clean
+sudo apt-get update
+sudo apt-get upgrade -y
 
-sudo apt clean
-sudo apt update
-sudo apt upgrade sim7600prg -y
-
-echo -e "\n--- Get System ---\n"
+echo -e "\n--- Get System ---"
 
 file1="/home/pi/startup/SysRelease"
 file2="/home/pi/startup/NewSysRelease"
@@ -51,18 +44,21 @@ if curl http://${APT_SERVER_IP}/apt-repo/system/Release > NewSysRelease;
 then
     if cmp -s "$file1" "$file2"; then
         echo -e "\n--- No updates ---\n"
+        rm NewSysRelease
     else
         VERSION=$(cat NewSysRelease | grep "Version")
         VERSION=${VERSION:9}
         echo -e "\n--- New version ${VERSION} available ---\n"
 
-        cat NewSysRelease > SysRelease
-        rm NewSysRelease
-
         curl http://${APT_SERVER_IP}/apt-repo/system/sim7600system_${VERSION}.tar.gz > flashdata.tar.gz
         mkdir -p /home/pi/FlashData
         rm /home/pi/FlashData/*
         tar xzfv flashdata.tar.gz -C /home/pi/FlashData/
+
+        rm flashdata.tar.gz
+
+        cat NewSysRelease > SysRelease
+        rm NewSysRelease
     fi
 fi
 
