07f2f84d47351446faf3addcedf82f95ab751419
KarenBarsegyan <karen.barsegyan-2001@bk.ru>
2023-05-25 13:59:50 +0300

    Now time issue is really closed

--- Flasher.py
+++ Flasher.py
@@ -8,7 +8,7 @@ import time
 import os
 import fcntl
 
-VERSION = '1.0.2'
+VERSION = '1.0.3'
 
 log = logger(__name__, logger.INFO, indent=75)
 log_status = logger('FlashStatuses', logger.INFO, indent=75)
